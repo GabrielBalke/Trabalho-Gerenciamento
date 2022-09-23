33ef37df487fcadd31a940e30d92bc04d2325782
GabrielFerreira39 <gabrielmferreira11@gmail.com>
2022-09-23 18:03:32 -0300

    atividade 6

---
+++ src/main/java/beecrowd/Main.java
@@ -0,0 +1,24 @@
+package beecrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int cont = 0;
+        double soma = 0;
+        double n = 0;
+        while (n >= 0) {
+            n = in.nextInt();
+            if (n >= 0) {
+                cont++;
+                soma += n;
+            }
+        }
+        double media = soma / cont;
+        System.out.println(String.format("%.2f", media));
+    }
+
+}
